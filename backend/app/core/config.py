a9b6d4e3d82f80716926b229989c75f63760b5d8
hieudoan7 <hieudoan190598@gmail.com>
2026-07-21 13:59:23 +0700

    make running app flexible betw local and docker

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_FILE = os.getenv("ENV_FILE", ".env.local")
+
 
 class Settings(BaseSettings):
     app_name: str = "KnowledgeHub API"
@@ -7,15 +13,13 @@ class Settings(BaseSettings):
     debug: bool = False
 
     database_url: str
-
     secret_key: str
-
     access_token_expire_minutes: int = 30
 
     model_config = SettingsConfigDict(
-        env_file=".env",
-        case_sensitive=False
+        env_file=Path(ENV_FILE),
+        case_sensitive=False,
     )
 
-settings = Settings()
 
+settings = Settings()
